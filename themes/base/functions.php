c105c2a338d05b59caed7adcc368fd109e406284
Tom Maitland <tommaitland@me.com>
2014-09-30 18:56:45 +1000

    Wrap embeds in div for responsive styles

--- themes/base/functions.php
+++ themes/base/functions.php
@@ -118,3 +118,14 @@ function mce_before_init( $settings ) {
 }
 
 add_filter('tiny_mce_before_init', 'mce_before_init');
+
+
+// Responsive Embeds
+// -------------------------
+
+function base_responsive_embeds($html, $url, $attr, $post_id) {
+	
+  return '<div class="embed-container">' . $html . '</div>';
+  
+}
+add_filter('embed_oembed_html', 'base_responsive_embeds', 99, 4);
